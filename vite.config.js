991f447680e83a026da6580064c54c2cabcb6835
gabrielandriotti <gabriel.andriotti.miguel@hotmail.com>
2025-12-18 15:38:41 -0300

    Update base URL for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-    base: '/1fan-landing/',
+    base: '/Landing_page_1Fan/',
     plugins: [react(), tailwindcss()],
 })
